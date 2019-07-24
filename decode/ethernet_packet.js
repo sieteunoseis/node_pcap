313f568839a28d9dc55d1011e633201e3c58f06d
Jeremy Worden <jeremy.worden@gmail.com>
2019-07-24 12:13:00 -0700

    Update for Cisco CDP detection

--- decode/ethernet_packet.js
+++ decode/ethernet_packet.js
@@ -13,7 +13,7 @@ function EthernetPacket(emitter) {
     this.payload = null;
 }
 
-EthernetPacket.prototype.decode = function (raw_packet, offset) {
+EthernetPacket.prototype.decode = function(raw_packet, offset) {
     this.dhost = new EthernetAddr(raw_packet, offset);
     offset += 6;
     this.shost = new EthernetAddr(raw_packet, offset);
@@ -31,54 +31,65 @@ EthernetPacket.prototype.decode = function (raw_packet, offset) {
     }
 
     if (this.ethertype < 1536) {
-        // this packet is actually some 802.3 type without an ethertype
-        this.ethertype = 0;
+        if (this.ethertype === 0xB6 || this.ethertype === 0xBB || this.ethertype === 0xBA || this.ethertype === 0xBE) { // CDP
+            this.payload = "Cisco Discovery Protocol"
+        } else {
+            this.payload = ""
+            // this packet is actually some 802.3 type without an ethertype
+            console.log("node_pcap: EthernetFrame() - Don't know how to decode ethertype " + this.ethertype);
+        }
     } else {
         // http://en.wikipedia.org/wiki/EtherType
         switch (this.ethertype) {
-        case 0x800: // IPv4
-            this.payload = new IPv4(this.emitter).decode(raw_packet, offset);
-            break;
-        case 0x806: // ARP
-            this.payload = new Arp(this.emitter).decode(raw_packet, offset);
-            break;
-        case 0x86dd: // IPv6 - http://en.wikipedia.org/wiki/IPv6
-            this.payload = new IPv6(this.emitter).decode(raw_packet, offset);
-            break;
-        case 0x88cc: // LLDP - http://en.wikipedia.org/wiki/Link_Layer_Discovery_Protocol
-            this.payload = "need to implement LLDP";
-            break;
-        default:
-            console.log("node_pcap: EthernetFrame() - Don't know how to decode ethertype " + this.ethertype);
+            case 0x800: // IPv4
+                this.payload = new IPv4(this.emitter).decode(raw_packet, offset);
+                break;
+            case 0x806: // ARP
+                this.payload = new Arp(this.emitter).decode(raw_packet, offset);
+                break;
+            case 0x86dd: // IPv6 - http://en.wikipedia.org/wiki/IPv6
+                this.payload = new IPv6(this.emitter).decode(raw_packet, offset);
+                break;
+            case 0x88cc: // LLDP - http://en.wikipedia.org/wiki/Link_Layer_Discovery_Protocol
+                this.payload = "need to implement LLDP";
+                break;
+            case 0x8922: // Ethernet II
+                this.payload = "Ethernet II";
+                break;
+            default:
+                console.log("node_pcap: EthernetFrame() - Don't know how to decode ethertype " + this.ethertype);
         }
     }
-
     return this;
 };
 
 EthernetPacket.prototype.decoderName = "ethernet-packet";
 EthernetPacket.prototype.eventsOnDecode = false;
 
-EthernetPacket.prototype.toString = function () {
+EthernetPacket.prototype.toString = function() {
     var ret = this.shost + " -> " + this.dhost;
     if (this.vlan) {
         ret += " vlan " + this.vlan;
     }
+
     switch (this.ethertype) {
-    case 0x800:
-        ret += " IPv4";
-        break;
-    case 0x806:
-        ret += " ARP";
-        break;
-    case 0x86dd:
-        ret += " IPv6";
-        break;
-    case 0x88cc:
-        ret += " LLDP";
-        break;
-    default:
-        ret += " ethertype " + this.ethertype;
+        case 0x800:
+            ret += " IPv4";
+            break;
+        case 0x806:
+            ret += " ARP";
+            break;
+        case 0x86dd:
+            ret += " IPv6";
+            break;
+        case 0x88cc:
+            ret += " LLDP";
+            break;
+        case 0xB6:
+            ret += " CDP";
+            break;
+        default:
+            ret += " ethertype " + this.ethertype;
     }
     return ret + " " + this.payload.toString();
 };
